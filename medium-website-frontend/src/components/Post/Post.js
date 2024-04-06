3aca0b8b0327fe4f114ca8360e7c151cdd5dc8bb
Gamze Seyit <gmzzsyyt@gmail.com>
2024-04-06 16:36:29 +0300

    css operations for post page

--- medium-website-frontend/src/components/Post/Post.js
+++ medium-website-frontend/src/components/Post/Post.js
@@ -4,8 +4,13 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom'
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
+
 
 function Post() {
+    const navigate = useNavigate();
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [postList, setPostList] = useState([]);
@@ -34,23 +39,30 @@ function Post() {
         return (
             <div>
                 {postList.map(post => (
-                    <Card key={post.id} sx={{ minWidth: 275 }}>
-                        <CardContent>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                User name: {post.userName}
-                                {console.log(post.userName)}
-                            </Typography>
-                            <Typography variant="h5" component="div">
-                                {post.title}
-                            </Typography>
-                            <Typography variant="body2">
-                                {post.text}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>
+                    <div key={post.id} style={{ marginBottom: '20px' }}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                {/* Avatar, User name, and Post title */}
+                                <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px', marginRight:'20px' }}>
+                                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                                        <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+                                        <Typography variant="h6" component="div" style={{ marginLeft: '10px' }}>
+                                            User name
+                                        </Typography>
+                                    </div>
+                                    <Typography style={{ display: 'flex', justifyContent: 'start', marginTop: '20px', marginLeft:'20px' }} variant="h5" component="div">
+                                        {post.title}
+                                    </Typography>
+                                </div>
+                                <Typography style={{ display: 'flex', justifyContent: 'start', marginTop: '10px', marginLeft:'20px' }} variant="body2">
+                                    {post.text}
+                                </Typography>
+                            </CardContent>
+                            <CardActions style={{ display: "flex", justifyContent: "flex-end", alignItems: "flex-end" }}>
+                                <Button size="small" onClick={() => navigate(`postDetail/${post.id}`)}>Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </div>
                 ))}
             </div>
         );
