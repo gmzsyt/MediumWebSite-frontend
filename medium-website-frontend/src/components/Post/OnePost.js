56dd7b5e18a020618816f918d1a1cc82fb82d729
Gamze Seyit <gmzzsyyt@gmail.com>
2024-05-23 11:53:47 +0300

    card added

--- medium-website-frontend/src/components/Post/OnePost.js
+++ medium-website-frontend/src/components/Post/OnePost.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPost } from '../../redux/Post/PostSlice';
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
 import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+
 
 const OnePost = ({ postId }) => {
     const dispatch = useDispatch();
@@ -16,18 +19,26 @@ const OnePost = ({ postId }) => {
 
     return (
         <div>
-            <Card sx={{ maxWidth: 800, marginLeft: 10 }}>
-                <CardActionArea>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {post.title}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            { post.text} 
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
+            <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px', marginRight:'20px' }}>
+                                    <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                                        <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+                                        <Typography variant="h6" component="div" style={{ marginLeft: '10px' }}>
+                                            {console.log(post.userName)}
+                                        </Typography>
+                                    </div>
+                                    <Typography style={{ display: 'flex', justifyContent: 'start', marginTop: '20px', marginLeft:'20px' }} variant="h5" component="div">
+                                        {post.title}
+                                    </Typography>
+                                </div>
+                                <Typography style={{ display: 'flex', justifyContent: 'start', marginTop: '10px', marginLeft:'20px' }} variant="body2">
+                                    {post.text}
+                                </Typography>
+                            </CardContent>
+                            <CardActions style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            </CardActions>
+                        </Card>
         </div>
     );
 };
